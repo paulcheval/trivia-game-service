57cbeec194455ca63505a1e4a398bf0235e3b8fc
paulchev1994 <paulchev@gmail.com>
2024-04-15 15:02:18 -0400

    First Commit

---
+++ src/main/java/com/freeloader/conversionservice/TriviaGameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.freeloader.conversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaGameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freeloader/conversionservice/TriviaGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeloader.conversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
